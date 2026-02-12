aecb9166935a686b3c6c1dc1629600fccf898829
JonChiu-programs <jonwchiu@gmail.com>
2026-02-11 21:45:11 -0500

    Reflection 5 changes

--- src/my-card.js
+++ src/my-card.js
@@ -18,20 +18,48 @@ export class MyCard extends LitElement {
     this.image = "https://upload.wikimedia.org/wikipedia/commons/d/da/1-Light_glyph.png";
     this.alt = "Light Glyph from The Owl House";
     this.link = "https://hax.psu.edu";
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
-      .card {
-  width: 700px;
-  height: 800px;
+    :host([fancy]) {
+    display: inline-block;
+    background-color: pink;
+    border: 2px solid fuchsia;
+  }
+   details summary {
+    text-align: center;
+    font-size: 20px;
+  }
+
+  details[open] summary {
+    font-weight: bold;
+  }
+  
+  details div {
+    border: 2px solid black;
+    //font-size: 20px;
+    text-align: center;
+    padding: 5px;
+    height: 90px;
+    overflow: auto;
+  }
+  .wrapper{
+    display: inline-block;
+  }
+  .card {
+  width: 400px;
+  height: 500px;
   background-color: var(--my-card-card-background-color, #eadb8fff); 
   margin: 50px;
   border: 2px solid black;
 }
 .glyph {
+  background-color: var(--my-card-glyph-background-color, #eadb8fff);
+  display: inline-block;
   width: 0px;
-  height: 00px;
+  height: 0px;
   margin: -30px 80px;
   border: none;
   text-align:center;
@@ -42,17 +70,18 @@ export class MyCard extends LitElement {
 .text {
   background-color: var(--my-card-text-background-color, #eadb8fff);
   text-align: center;
-  font-size: 50px;
+  font-size: 25px;
   font-family:times new roman;
-  padding: 10px;
 }
 
 button img{
   background-color: var(--my-card-button-img-background-color, #eadb8fff);
-    margin: -5px 13px;
-    width: 500px;
-    height: 500px;
+    margin: 35px -35px;
+    padding: 5px;
+    width: 300px;
+    height: 300px;
   }
+  
 
 @media (min-width: 500px) and (max-width: 800px){
   .card {
@@ -102,13 +131,35 @@ button img{
 }
 `;
   }
+    openChanged(e) {
+    console.log(e);
+    if (e.target.getAttribute('open') !== null) {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    };
+  }
   render() {
     return html`
   <div class="wrapper">
     <div class="card">
       <section class="text">
-        <slot></slot>
-      </section>
+        <!--<details ?open="${this.fancy}">
+          <summary>Description</summary>
+            <div>
+              <slot>${this.description}</slot>
+            </div>
+        </details>-->
+      
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+          <summary>Description</summary>
+            <div>
+              <slot>${this.description}</slot>
+            </div>
+          </details>
+
+        </section>
     
       <section class="button-section">
         <a href= "${this.link}">
@@ -128,6 +179,7 @@ button img{
       image: { type: Image },
       alt: { type: String },
       link: { type: String },
+      fancy: { type: Boolean, reflect: true }
     };
   }
 }
